0db768765bc74595df5876fbb98d96b1d7f84c98
Marcio Goda <marciogoda@gmail.com>
2020-08-19 16:42:53 +0100

    Fixed attribution

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_region" "current" {}
 
 
 data "aws_s3_bucket_object" "platform_config" {
-  bucket = var.bucket
+  bucket = "${var.bucket}"
   key = "${data.aws_iam_account_alias.current.account_alias}/${data.aws_region.current.name}.json"
 }
 
