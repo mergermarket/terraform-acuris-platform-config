c1f3339c2d71661b586c11cee6271c27bd4390dd
Marcio Goda <marciogoda@gmail.com>
2020-08-19 15:38:34 +0100

    Added platform_config data source

---
+++ main.tf
@@ -0,0 +1,12 @@
+data "aws_iam_account_alias" "current" {}
+data "aws_region" "current" {}
+
+
+data "aws_s3_bucket_object" "platform_config" {
+  bucket = var.bucket
+  key = "${data.aws_iam_account_alias.current.account_alias}/${data.aws_region.current.name}.json"
+}
+
+output "config" {
+    value = data.aws_s3_bucket_object.platform_config.body
+}
