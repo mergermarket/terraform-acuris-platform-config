1f7e808c3fea50db3e6aeeae5292dd70cf3dcf47
Marcio Goda <marciogoda@gmail.com>
2025-01-30 10:43:21 +0000

    Use current region (#6)
    * update to use current region
    
    * fix config file region read
    
    ---------
    
    Co-authored-by: Marcio Goda <marcio.goda@acuris.com>

--- main.tf
+++ main.tf
@@ -1,18 +1,21 @@
+locals {
+  region = var.platform_config_region != "" ? var.platform_config_region : data.aws_region.current.name
+  platform_prefix = (var.datacenter == "" ? data.aws_iam_account_alias.current.account_alias : var.datacenter)
+}
+
 provider "aws" {
   alias = "platform_config_bucket"
   region = "eu-west-1"
 }
 
-locals {
-    platform_prefix = (var.datacenter == "" ? data.aws_iam_account_alias.current.account_alias : var.datacenter)
-}
+data "aws_region" "current" {}
 
 data "aws_iam_account_alias" "current" {}
 
 data "aws_s3_object" "platform_config" {
   provider = aws.platform_config_bucket
   bucket = var.bucket
-  key = "${local.platform_prefix}/${var.platform_config_region}.json"
+  key = "${local.platform_prefix}/${local.region}.json"
 }
 
 output "config" {
